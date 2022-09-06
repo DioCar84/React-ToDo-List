238d7243cf63b265af819994170ab849b2ddc248
DioCar84 <82677312+DioCar84@users.noreply.github.com>
2022-09-06 09:46:20 +0100

    Updates

---
+++ precache-manifest.dffe9556e3e4054faf51758da4b6879c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f81e4bc5b0c42ad53334",
+    "url": "/React-ToDo-List/static/css/main.ce7f3c1c.chunk.css"
+  },
+  {
+    "revision": "f81e4bc5b0c42ad53334",
+    "url": "/React-ToDo-List/static/js/main.f81e4bc5.chunk.js"
+  },
+  {
+    "revision": "06de82619331a440d5d4",
+    "url": "/React-ToDo-List/static/js/1.06de8261.chunk.js"
+  },
+  {
+    "revision": "62be3cd717dba316589e",
+    "url": "/React-ToDo-List/static/js/runtime~main.62be3cd7.js"
+  },
+  {
+    "revision": "99fa4268bb0ac123cb0428a7df56c6b0",
+    "url": "/React-ToDo-List/index.html"
+  }
+];
